066abe007dc64915a4a0b39c75cd524123f2721a
kylercantu <kylercantu99@gmail.com>
2023-10-29 19:15:58 -0400

    added documentation

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ import usedCars from "./usedCars.js";
 
 const carGridContainer = document.getElementById("car-grid-container");
 
+// Function that creates all individual car cards that the user see's
 function createCarCards() {
   usedCars.forEach((car) => {
     //creates a div element/container for each car
@@ -25,13 +26,16 @@ function createCarCards() {
   });
 } //end of createCards()
 
+// Call to the createCarCads function to populate the page with the cars available
 createCarCards();
 
 /*---------------Store radio button Choices Code Section---------------------*/
-let choiceArr = [];
+
+let choiceArr = []; //Stores the user's selected filtering options
 const radioButtons = document.querySelectorAll('input[type="radio"]'); //List of all radio buttons
 console.log(radioButtons);
-//Function to store the values of the selected radio buttons
+
+//Function to store the values of the selected radio buttons and stores them in the array
 function storeChoices() {
   choiceArr.length = 0; //Resets the array
   radioButtons.forEach((button) => {
@@ -48,8 +52,9 @@ radioButtons.forEach((button) => {
 });
 
 /*---------------Filter Cars Code Section------------------------------------*/
+
 const applyBtn = document.getElementById("apply-button");
-const carDiv = document.querySelectorAll(".car-card"); //List of all of the car-card divs
+const carDiv = document.querySelectorAll(".car-card"); //List of all the car-card divs
 const resetBtn = document.getElementById("reset-button");
 
 //Function to filter the cars based on the values in the array that includes the checkboxes that are checked
@@ -57,7 +62,7 @@ function filterCars() {
   /*
   1) Iterate through each car div (card)
   2) First check if the array is empty, if it is, then no radio buttons were selected, therefore show all cars (cards)
-  3) Else, check to see if every word in the array is contained in each car card, if it is, then only display that card 
+  3) Else, check to see if every word in the array is contained in each car card, if it is, then only display the card(s) 
       that contains every word from the array
   4) If the car card doesn't contain every word, then don't show that card
 
@@ -75,6 +80,7 @@ function filterCars() {
   });
 } //end filterCars()
 
+//Function to reset all filtering choices when the user clicks the reset-filter button
 function resetFilter() {
   radioButtons.forEach((button) => {
     button.checked = false;
@@ -83,7 +89,7 @@ function resetFilter() {
   carDiv.forEach((card) => {
     card.style.display = "flex";
   });
-}
+} //end resetFilter()
 
 //Adds event listener to the apply and reset filter buttons
 applyBtn.addEventListener("click", filterCars);
